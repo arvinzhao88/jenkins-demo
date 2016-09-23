fa7475326683f8a53b55f5df83d37ab93eb67d5f
Jack Hu <hutufeia2009@sina.com>
2016-09-24 00:02:28 +0800

    modify mongodb

--- db/initdata.js
+++ db/initdata.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 var JenkinsDemo = mongoose.model('JenkinsDemo');
 
-// JenkinsDemo.countAsync().then(function (count) {
-//   if(count === 0){
-//     JenkinsDemo.createAsync({
-//       name:'jenkins-demo',
-//       count: 1
-//     })
-//   }
-// })
+JenkinsDemo.countAsync().then(function (count) {
+  if(count === 0){
+    JenkinsDemo.createAsync({
+      name:'jenkins-demo',
+      count: 1
+    })
+  }
+})
