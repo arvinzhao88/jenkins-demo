fd4b111d1d7072f4270b091e8deb13991c0c36e7
Jack Hu <hutufeia2009@sina.com>
2016-09-25 19:40:17 +0800

    first mongo connect

--- db/initdata.js
+++ db/initdata.js
@@ -2,10 +2,12 @@ var mongoose = require('mongoose');
 var JenkinsDemo = mongoose.model('JenkinsDemo');
 
 JenkinsDemo.countAsync().then(function (count) {
-  if(count === 0){
-    JenkinsDemo.createAsync({
-      name:'jenkins-demo',
-      count: 1
-    })
+  if(count < 1){
+    JenkinsDemo.removeAsync().then(function () {
+      JenkinsDemo.createAsync({
+        name:'jenkins-demo',
+        count: 1
+      })
+    });
   }
 })
